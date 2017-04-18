78b8d50a6fa7f5f5671023b3adc99c07e39e3c6f
Robert Badea <owlree128@gmail.com>
2017-04-18 17:06:19 +0000

    fixed paranthesis

--- catpol/pipelines/mongo.py
+++ catpol/pipelines/mongo.py
@@ -14,22 +14,21 @@ class MongoDBPipeline(object):
         logger.debug(
             'Connected to MongoDB {}:{}'.format(
                 settings['MONGODB_SERVER'],
-                settings['MONGODB_PORT'])
+                settings['MONGODB_PORT']))
         db = client[settings['MONGODB_DB']]
         logger.debug(
             'Accessed database {}'.format(
-                settings['MONGODB_DB'])
+                settings['MONGODB_DB']))
         db.authenticate(
             settings['MONGODB_USER'], 
             settings['MONGODB_PASS'],
-            mechanism = settings['MONGODB_MECH']
-        )
+            mechanism = settings['MONGODB_MECH'])
         logger.debug('Authenticated!')
         self.collection = db[settings['MONGODB_COLLECTION']]
         logger.debug(
             'Accessed collection {} in database {}'.format(
                 settings['MONGODB_COLLECTION'],
-                settings['MONGODB_DB'])
+                settings['MONGODB_DB']))
 
     def process_item(self, item, spider):
         self.collection.insert(dict(item))
