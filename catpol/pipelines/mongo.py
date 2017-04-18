a44c26bd3e2c008ed966af68198462da4163e172
Robert Badea <owlree128@gmail.com>
2017-04-18 16:48:06 +0000

    added mongodb support

---
+++ catpol/pipelines/mongo.py
@@ -0,0 +1,39 @@
+import pymongo
+import logging
+
+from scrapy.conf import settings
+from scrapy.exceptions import DropItem
+from scrapy import log
+
+class MongoDBPipeline(object):
+    def __init__(self):
+        client = pymongo.MongoClient(
+            settings['MONGODB_SERVER'],
+            settings['MONGODB_PORT']
+        )
+        logger.debug(
+            'Connected to MongoDB {}:{}'.format(
+                settings['MONGODB_SERVER'],
+                settings['MONGODB_PORT'])
+        db = client[settings['MONGODB_DB']]
+        logger.debug(
+            'Accessed database {}'.format(
+                settings['MONGODB_DB'])
+        db.authenticate(
+            settings['MONGODB_USER'], 
+            settings['MONGODB_PASS'],
+            mechanism = settings['MONGODB_MECH']
+        )
+        logger.debug('Authenticated!')
+        self.collection = db[settings['MONGODB_COLLECTION']]
+        logger.debug(
+            'Accessed collection {} in database {}'.format(
+                settings['MONGODB_COLLECTION'],
+                settings['MONGODB_DB'])
+
+    def process_item(self, item, spider):
+        self.collection.insert(dict(item))
+        logger = logging.getLogger(__name__)
+        logger.debug('Added item to mongo database')
+        return item
+        
--- catpol/settings.py
+++ catpol/settings.py
@@ -6,3 +6,13 @@ ROBOTSTXT_OBEY = True
 LOG_LEVEL = 'INFO'
 
 COMMANDS_MODULE = 'catpol.commands'
+
+ITEM_PIPELINES = {'catpol.pipelines.MongoDBPipeline': 300}
+
+MONGODB_SERVER = 'localhost'
+MONGODB_PORT = 27017 
+MONGODB_DB = 'db'
+MONGODB_COLLECTION = 'collection'
+MONGODB_USER = 'user'
+MONGODB_PASS = 'pass'
+MONGODB_MECH = 'SCRAM-SHA-1'
