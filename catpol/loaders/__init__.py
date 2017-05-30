3c0f12a697d38b9c3c6bf5a646d1e00d6ad814f6
Robert Badea <owlree128@gmail.com>
2017-05-31 00:50:12 +0300

    added plenery time entries

--- catpol/loaders/__init__.py
+++ catpol/loaders/__init__.py
@@ -2,3 +2,4 @@ from catpol.loaders.activity import *
 from catpol.loaders.initiative import *
 from catpol.loaders.status import *
 from catpol.loaders.link import *
+from catpol.loaders.plenerytime import *
