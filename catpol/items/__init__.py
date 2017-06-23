701dfc80c6c57c74692d8d04a818b80779169079
Costin Bleotu <costi@databus.pro>
2017-06-23 11:34:42 +0300

    Added votes and voting

--- catpol/items/__init__.py
+++ catpol/items/__init__.py
@@ -4,3 +4,5 @@ from catpol.items.link import *
 from catpol.items.status import *
 from catpol.items.plenerytime import *
 from catpol.items.county import *
+from catpol.items.voting import *
+from catpol.items.votes import *
--- catpol/loaders/__init__.py
+++ catpol/loaders/__init__.py
@@ -3,3 +3,5 @@ from catpol.loaders.initiative import *
 from catpol.loaders.status import *
 from catpol.loaders.link import *
 from catpol.loaders.plenerytime import *
+from catpol.loaders.voting import *
+from catpol.loaders.votes import *
