20528743408cec50f71b31dbf4345abf02134fe5
Costin Bleotu <costi@databus.pro>
2017-09-29 16:51:33 +0300

    crwal person url

--- catpol/spiders/cdep_voting.py
+++ catpol/spiders/cdep_voting.py
@@ -7,6 +7,8 @@ import catpol.loaders as loaders
 import catpol.items as items
 import catpol.http as http
 
+from bs4 import BeautifulSoup as soup
+
 
 class CdepVoting(scrapy.Spider):
     name = 'cdep_voting'
@@ -85,6 +87,7 @@ class CdepVoting(scrapy.Spider):
     def parse_month(self, response):
         for day in response.body.decode().split(','):
             if day:
+                print(day)
                 url = str(
                         'http://www.cdep.ro/pls/steno/evot2015.xml?'
                         'par1=1&par2='+day
@@ -107,13 +110,45 @@ class CdepVoting(scrapy.Spider):
                 voting_loader.add_value('vote_no', vote.get('AU_VOTAT_NU',''))
                 voting_loader.add_value('vote_ab', vote.get('AU_VOTAT_AB',''))
 
+                # url = str(
+                #     'http://www.cdep.ro/pls/steno/evot2015.xml?'
+                #     'par1=2&par2='+vote['VOTID']
+                # )
                 url = str(
-                    'http://www.cdep.ro/pls/steno/evot2015.xml?'
-                    'par1=2&par2='+vote['VOTID']
+                    'http://www.cdep.ro/pls/steno/evot2015.Nominal?'
+                    'idv='+vote['VOTID']
                 )
-                yield http.Reqo(url=url, callback=self.parse_vote, meta={'voting_loader': voting_loader})
+                yield http.Reqo(url=url, callback=self.parse_vote, meta={'voting_loader': voting_loader, 'vote_id': vote['VOTID']})
 
     def parse_vote(self, response):
+        voting_loader = response.meta['voting_loader']
+        if response.body:
+            root = soup(response.body, 'html.parser')
+            voting_table = root.find_all('table')[-1]
+            votes_list = []
+            vote_id = response.meta['vote_id']
+            header = True
+            for vote in voting_table.find_all('tr'):
+                if header:
+                    header = False
+                    continue
+
+                person_url = 'http://www.cdep.ro' + vote.find_all('td')[1].find('a')['href']
+                person = vote.find_all('td')[1].find('a').text
+                party = vote.find_all('td')[2].text
+                vote_val = vote.find_all('td')[3].text.strip()
+
+                votes_loader = loaders.VotesLoader(items.VotesItem())
+                votes_loader.add_value('vote_id', vote_id)
+                votes_loader.add_value('person', person)
+                votes_loader.add_value('person_url', person_url)
+                votes_loader.add_value('party', party)
+                votes_loader.add_value('vote', vote_val)
+                votes_list.append(votes_loader.load_item())
+            voting_loader.add_value('votes', votes_list)
+        yield voting_loader.load_item()
+
+    def parse_vote_xml(self, response):
         voting_loader = response.meta['voting_loader']
         if response.body:
             response_dict = xmltodict.parse(response.body)
