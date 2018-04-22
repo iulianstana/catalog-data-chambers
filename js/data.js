f4001e82f98834a2a43edc2e61cb7b0881441428
cristian-marin-hs <cristian.marin@hootsuite.com>
2018-04-22 22:31:57 +0300

    Format data from api to use it in the d3 chart

--- js/data.js
+++ js/data.js
@@ -1,16 +1,13 @@
 const name = "Ivan Deputatu'";
 
 var timeline_data = [
-  {time: new Date(1992, 4,25), name: 'Joined PSD'},
-  {time: new Date(1995, 4,17), name: 'Left PSD'},
-  {time: new Date(1997, 4,25), name: 'Joined PNL'},
-  {time: new Date(1999, 4,19), name: 'Left PNL'},
-  {time: new Date(2008, 4,16), name: 'Joined ALDE'},
-  {time: new Date(2015, 4,19), name: 'Left ALDE'},
-  {time: new Date(2017,11,18), name: 'Joined PP-DD'},
+    {since: new Date(1992, 4,25), until: new Date(1997, 4,17), name: 'PSD'},
+    {since: new Date(1997, 4,25), until: new Date(1999, 4,19), name: 'PNL'},
+    {since: new Date(2008, 4,16), until: new Date(2015, 4,19), name: 'ALDE'},
+    {since: new Date(2017,11,18), until: null, name: 'PP-DD'},
 ];
 
 module.exports = {
-	name, 
-	timeline_data
+    name,
+    timeline_data
 };
--- js/getdata.js
+++ js/getdata.js
@@ -1,10 +1,24 @@
 var http = require("http");
+var querystring = require('querystring');
 var config = require("./config.js");
 
 options = config.getEnvironment()['data_service']
 
-function getJsonData(callback) {
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
 
+var name = getParameterByName('name');
+document.getElementById("name").innerHTML = name;
+
+function getJsonData(callback) {
+    options.path = options.path + window.location.search;
     return http.get(options, function(response) {
         var body = '';
         response.on('data', function(d) {
@@ -13,7 +27,6 @@ function getJsonData(callback) {
         response.on('end', function() {
             // Data reception is done, do whatever with it!
             var parsed = JSON.parse(body);
-            //console.log(parsed);
             callback(parsed);
         });
     });
